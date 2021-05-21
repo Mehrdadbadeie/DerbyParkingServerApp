f1935746697536105f5fb16be715d67fb8dea2e4
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-05-21 09:33:16 +0200

    This is just a test.

--- src/se/yrgo/domain/ParkingTicket.java
+++ src/se/yrgo/domain/ParkingTicket.java
@@ -9,7 +9,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-
+/**
+ * 
+ * This is a parking ticket.
+ * 
+ * @author Tommi
+ *
+ */
 @Entity
 public class ParkingTicket implements Serializable {
 	@Id
