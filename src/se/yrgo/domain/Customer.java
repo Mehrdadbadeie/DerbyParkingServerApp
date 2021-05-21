4e02a557dd845c3c8317aacfd37ad7a4bc6e27c0
Tommi Ahonen <tomaho1129@skola.goteborg.se>
2021-05-21 09:24:26 +0200

    Fixed bug in Customer class: @Entity was missing.

--- src/se/yrgo/domain/Customer.java
+++ src/se/yrgo/domain/Customer.java
@@ -2,10 +2,12 @@ package se.yrgo.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Customer implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
